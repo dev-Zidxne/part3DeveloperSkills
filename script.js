33d0e1d53762eff3dca41137293bb6d2e0ad052c
Zidane Innis <zidaneinnis@gmail.com>
2022-04-23 21:22:31 -0500

    Coding Challenge #1

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ const temperatures = [3, -2, -6, 1, "error", 9, 13, 17, 15, 14, 9, 5];
 // - How to ignore errors?
 // - Find max value in temp array
 // - SUbtract min from max (amplitude) and return it
+/*
 
+ 
 const calcTempAmplitude = function (temps) {
   let max = temps[0];
   let min = temps[0];
@@ -70,3 +72,65 @@ const calcTempAmplitudeNew = function (t1, t2) {
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 
 console.log(amplitudeNew);
+
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celcius",
+
+    // C) FIX
+    // value: Number(prompt("Degrees celcius:")),
+    value: 10,
+  };
+
+  // B) FIND
+  console.table(measurement);
+
+  //   console.log(measurement.value);
+  //   console.warn(measurement.value);
+  //   console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// A) IDENTIFY
+console.log(measureKelvin());
+
+//Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const array1 = ["a", "b", "c"];
+  const array2 = ["d", "e", "f"];
+  const array3 = array1.concat(array2);
+
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+
+console.log(amplitudeBug);
+*/
+
+const printForcast = function (oop) {
+  const arr = [17, 21, 23];
+  for (let i = 0; i < arr; i++) for (let y = 0; y < arr.length; y++) return arr;
+  console.log(`... ${y}C in ${i} days, ...`);
+};
+
+console.log(printForcast());
